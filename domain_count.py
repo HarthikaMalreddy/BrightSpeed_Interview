6f8ae84fb63f2531fc130edf027baf59f9c66354
Harthika Malreddy <harthikamalreddy@Harthikas-MBP.attlocal.net>
2024-10-25 12:46:24 -0700

    Implement count_domains function

--- domain_count.py
+++ domain_count.py
@@ -1,5 +1,15 @@
+from collections import defaultdict
 def count_domains(domain_list):
-    """
-    Your code goes in this block!!!!
-    """
-    return ""
+    # A defaultdict to count domain occurrences
+    counts = defaultdict(int)
+
+    for domain in domain_list:
+        if isinstance (domain, str) and domain: #Check if domain is a non-empty string
+            # Remove "www." if exists
+            clean_domain = domain[4:] if domain.startswith("www.") else domain
+            counts[clean_domain] += 1 # Increment count
+    # sort the counted domains by count in desc order
+    sorted_count = sorted(counts.items(), key = lambda x : x[1], reverse = True)
+    return sorted_count
+
+   
